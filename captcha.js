94efb9714030691e29bfc0e0c41f788537568589
Insanegames <103283478+OutRed@users.noreply.github.com>
2022-08-31 11:27:27 -0500

    Create captcha.js

---
+++ captcha.js
@@ -0,0 +1,8 @@
+<script src="https://www.google.com/recaptcha/enterprise.js?render=6Le82r4hAAAAAJruOCxdQXzaeqYXtA00PkBGJTQw"></script>
+<script>
+grecaptcha.enterprise.ready(function() {
+    grecaptcha.enterprise.execute('6Le82r4hAAAAAJruOCxdQXzaeqYXtA00PkBGJTQw', {action: 'login'}).then(function(token) {
+       ...
+    });
+});
+</script>
