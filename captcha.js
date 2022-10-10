47ca9c0a6f7586aaaa32431d7d4a08d44d8c38e1
Insanegames <103283478+OutRed@users.noreply.github.com>
2022-10-10 13:18:22 -0500

    Delete captcha.js

--- captcha.js
+++
@@ -1,8 +0,0 @@
-<script src="https://www.google.com/recaptcha/enterprise.js?render=6Le82r4hAAAAAJruOCxdQXzaeqYXtA00PkBGJTQw"></script>
-<script>
-grecaptcha.enterprise.ready(function() {
-    grecaptcha.enterprise.execute('6Le82r4hAAAAAJruOCxdQXzaeqYXtA00PkBGJTQw', {action: 'login'}).then(function(token) {
-       ...
-    });
-});
-</script>
