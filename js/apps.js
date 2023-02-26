8a5a351b7856bd12c0f4d46470b1383120eb7238
OutRed <103283478+OutRed@users.noreply.github.com>
2023-02-26 15:39:30 -0600

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -7,7 +7,7 @@ fetch('/js/json/apps.json')
     data.forEach(item => {
       const game = document.createElement('div');
       game.classList.add('game');
-      game.innerHTML = `<a href="${item.link}" target="_blank"><img src="${item.image}" alt="${item.title}"></a><h2><a href="${item.link}" target="_blank">${item.title}</a></h2>`;
+      game.innerHTML = `<a href="${item.link}"><img src="${item.image}" alt="${item.title}"></a><h2><a href="${item.link}">${item.title}</a></h2>`;
       gamesContainer.appendChild(game);
     });
   });
